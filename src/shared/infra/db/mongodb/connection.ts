441a937506ab880e9a90a851491d45a749a9d070
jigth <danielochoamontes@gmail.com>
2024-08-03 13:02:23 -0500

    CHORE: Add sane default for MongoDB conection URI

--- src/shared/infra/db/mongodb/connection.ts
+++ src/shared/infra/db/mongodb/connection.ts
@@ -1,6 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;
+// Sane default URI was provided as it was already exposed in the docker compose file.
+const uri = process.env.MONGODB_URI ?? 'mongodb://root:example@localhost:27017/backend-test?authSource=admin';
 
 export const mongodbClient = new MongoClient(uri);
 export const mongodbDatabase = mongodbClient.db('backend-test');
