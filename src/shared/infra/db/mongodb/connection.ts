9a5375843e45ba898448157da1ed6b4af3c28f1c
jigth <danielochoamontes@gmail.com>
2024-08-03 17:14:08 -0500

    CHORE: Add sane default to MongoDB connection setting.
    We already have a docker compose file exposing credentials for a dev environment so I used those as default in MongoDB.

--- src/shared/infra/db/mongodb/connection.ts
+++ src/shared/infra/db/mongodb/connection.ts
@@ -2,6 +2,7 @@ const { MongoClient } = require('mongodb');
 
 // Sane default URI was provided as it was already exposed in the docker compose file.
 const uri = process.env.MONGODB_URI ?? 'mongodb://root:example@localhost:27017/backend-test?authSource=admin';
+const db_name = process.env.MONGODB_DATABASE_NAME ?? 'backend-test';
 
 export const mongodbClient = new MongoClient(uri);
-export const mongodbDatabase = mongodbClient.db('backend-test');
+export const mongodbDatabase = mongodbClient.db(db_name);
